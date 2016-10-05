adc06d8b8ae065fada35af3a45c5b828cc4aa8cc
Daniel Franklin <soloneka@bellsouth.net>
2016-10-04 21:51:03 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
-## Put comments here that give an overall description of what your
+## Put comments here that give an overall description of what you
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the makeCacheMatrix if inverse has already
+## been calculated cache should retrive the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
